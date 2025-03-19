dff2bea50f69de9efe41b410963bf4b3aeafb9e1
Jikael Gagnon <jikaelgagnon@gmail.com>
2025-03-19 15:30:23 -0400

    Add mineral collected counter to UI

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerInventory : MonoBehaviour
 {
     public int NumberOfMinerals {get; private set;}
     public ArrayList Minerals = new ArrayList();
 
+    public UnityEvent<PlayerInventory> OnMineralCollected;
+
     public void MineralCollected(object mineral) {
         Minerals.Add(mineral);
         NumberOfMinerals++;
+        OnMineralCollected.Invoke(this);
     }
 }
