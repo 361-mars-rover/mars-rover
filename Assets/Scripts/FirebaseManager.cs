adb5b996fdafcd27f83d38b694340c2db78c7d54
cmgvc <chloe.gavv@gmail.com>
2025-03-20 12:51:38 -0400

    firebase config

--- Assets/Scripts/FirebaseManager.cs
+++ Assets/Scripts/FirebaseManager.cs
@@ -1,1 +1,57 @@
-//empty
+using Firebase;
+using Firebase.Database;
+using Firebase.Extensions;
+using UnityEngine;
+
+public class FirebaseManager : MonoBehaviour
+{
+    public static FirebaseManager Instance;
+    private DatabaseReference dbReference;
+    private bool isFirebaseInitialized = false;
+    void Start() {
+        // Get the root reference location of the database.
+        dbReference = FirebaseDatabase.DefaultInstance.RootReference;
+    }
+
+    void Awake()
+    {
+        dbReference = FirebaseDatabase.DefaultInstance.RootReference;
+    }
+
+    public void StoreMaterialData(string materialID)
+    {
+        MaterialData material = new MaterialData();
+        string json = JsonUtility.ToJson(material);
+        dbReference.Child("materials").Child(materialID).SetRawJsonValueAsync(json);
+    }
+}
+
+
+// rover data model
+[System.Serializable]
+public class Rover
+{
+    public string avatar;
+    public string brain;
+    
+    public Rover(string avatar, string brain)
+    {
+        this.avatar = avatar;
+        this.brain = brain;
+    }
+}
+
+// material data model
+[System.Serializable]
+public class MaterialData
+{
+    // public string type;
+    // public string collected_by;
+    
+    public MaterialData()
+    // public MaterialData(string type, string collected_by)
+    {
+        // this.type = type;
+        // this.collected_by = collected_by;
+    }
+}
