55b7d2347d7cc82fb5eb76b0c5c04b7fd9882628
Ethan Lim <ethan.lim@gmail.com>
2025-04-16 21:02:38 -0400

    Added Quit Functionality
    Quit Functionality and AI About Slide added. Note that the application quits during both pre and post build.

--- Assets/Scripts/Menu/UI/WelcomeScreen.cs
+++ Assets/Scripts/Menu/UI/WelcomeScreen.cs
@@ -2,11 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class WelcomeScreen : MonoBehaviour
 {
     public void StartSimulation()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        //Debug.Log("Quit button pressed!");
+
+        #if UNITY_EDITOR
+            EditorApplication.isPlaying = false; //when simulating in Unity Editor, will stop playing/simulates quiting application
+        #else
+            Application.Quit(); //will quit in build
+        #endif
     }
 }
